973bdd7784492f355e17efce077b64bcedb36696
eric chuderewicz <echuderewicz@gmail.com>
2018-05-08 18:08:02 -0400

    first commit

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function(){
+
+  browserSync.init({
+    //this removed the annoying/distracting black button
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', function(){
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('cssInject');
+  })
+
+  watch('./app/assets/scripts/**/*.js', function(){
+    gulp.start('scriptsRefresh');
+  })
+
+});
+
+//inject css into during
+//note the 2nd argument which indicates the dependencies
+//that need to run before cssInject
+
+gulp.task('cssInject', ['styles'], function(){
+  //the temp styles has be generated prior to piping into browser-sync
+  return gulp.src('./app/temp/styles/styles.css')
+  .pipe(browserSync.stream());
+})
+
+gulp.task('scriptsRefresh', ['scripts'], function(){
+  browserSync.reload();
+})
